01061142465c17e5fdbfeed6e009d663beb0d8cb
Yegor Bugayenko <yegor256@gmail.com>
2022-03-31 07:46:51 +0300

    #1 start

---
+++ src/main/java/com/yegor256/xsline/Xsline.java
@@ -0,0 +1,97 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.yegor256.xsline;
+
+import com.jcabi.xml.XML;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Chain of XSL transformations.
+ *
+ * <p>Use it like this:
+ *
+ * <pre> XML output = new Xsline()
+ *   .with(StXSL(new XSLDocument("...")));
+ *   .with(StXSL(new XSLDocument("...")));
+ *   .pass(input);
+ * </pre>
+ *
+ * <p>See all implementations of {@link Shift} to learn the functionality
+ * this package provides.</p>
+ *
+ * @since 0.1.0
+ */
+public final class Xsline {
+
+    /**
+     * Collection of shifts.
+     */
+    private final Collection<Shift> shifts;
+
+    /**
+     * Ctor.
+     */
+    public Xsline() {
+        this(new ArrayList<>(0));
+    }
+
+    /**
+     * Ctor.
+     * @param list List of shifts
+     */
+    public Xsline(final Collection<Shift> list) {
+        this.shifts = Collections.unmodifiableCollection(list);
+    }
+
+    /**
+     * With this new shift.
+     * @param shift The shift
+     */
+    public Xsline with(final Shift shift) {
+        final Collection<Shift> list = new ArrayList<>(this.shifts.size() + 1);
+        list.addAll(this.shifts);
+        list.add(shift);
+        return new Xsline(list);
+    }
+
+    /**
+     * Run it all with the given XML.
+     * @param input The input XML
+     * @return The output XML
+     */
+    public XML pass(final XML input) {
+        XML before = input;
+        XML after = before;
+        int pos = 0;
+        for (final Shift shift : this.shifts) {
+            after = shift.apply(pos, before);
+            ++pos;
+            before = after;
+        }
+        return after;
+    }
+
+}
---
+++ src/test/java/com/yegor256/xsline/XslineTest.java
@@ -0,0 +1,61 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.yegor256.xsline;
+
+import com.jcabi.matchers.XhtmlMatchers;
+import com.jcabi.xml.XML;
+import com.jcabi.xml.XMLDocument;
+import com.jcabi.xml.XSL;
+import com.jcabi.xml.XSLDocument;
+import java.io.IOException;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test case for {@link Xsline}.
+ *
+ * @since 0.1.0
+ */
+public final class XslineTest {
+
+    @Test
+    public void simpleScenario() throws IOException {
+        final XSL xsl = new XSLDocument(
+            this.getClass().getResource("/com/yegor256/xsline/add-brackets.xsl")
+        );
+        final XML output = new Xsline()
+            .with(
+                new StRepeated(
+                    new StXSL(xsl),
+                    xml -> xml.nodes("/x[starts-with(., '{{')]").isEmpty()
+                )
+            )
+            .pass(new XMLDocument("<x>hello</x>"));
+        MatcherAssert.assertThat(
+            output,
+            XhtmlMatchers.hasXPaths("/x[.='{{hello}}']")
+        );
+    }
+
+}
