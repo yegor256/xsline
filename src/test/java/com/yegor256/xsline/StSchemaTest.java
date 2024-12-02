ff7b033fb40f5e0d409edbe868673473bbef2d89
Yegor Bugayenko <yegor256@gmail.com>
2024-12-02 07:41:43 +0300

    #132 skip on win

--- src/test/java/com/yegor256/xsline/StSchemaTest.java
+++ src/test/java/com/yegor256/xsline/StSchemaTest.java
@@ -31,6 +31,8 @@ import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 /**
  * Test case for {@link StSchema}.
@@ -60,6 +62,7 @@ final class StSchemaTest {
     }
 
     @Test
+    @DisabledOnOs(OS.WINDOWS)
     void validatesWithoutSchema() {
         final Path xsd = Paths.get("src/test/resources/com/yegor256/xsline/simple.xsd");
         Assumptions.assumeTrue(xsd.toFile().exists());
