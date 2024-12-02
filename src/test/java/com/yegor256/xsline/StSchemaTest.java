46f9eafec175250ed1cd7a7b9e333be40d22e509
Yegor Bugayenko <yegor256@gmail.com>
2024-12-02 07:35:50 +0300

    #132 test for StSchema

--- src/test/java/com/yegor256/xsline/StSchemaTest.java
+++ src/test/java/com/yegor256/xsline/StSchemaTest.java
@@ -25,8 +25,11 @@ package com.yegor256.xsline;
 
 import com.jcabi.matchers.XhtmlMatchers;
 import com.jcabi.xml.XMLDocument;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -53,6 +56,28 @@ final class StSchemaTest {
             () -> new Xsline(
                 new StSchema("/com/yegor256/xsline/simple.xsd")
             ).pass(new XMLDocument("<bar/>"))
+        );
+    }
+
+    @Test
+    void validatesWithoutSchema() {
+        final Path xsd = Paths.get("src/test/resources/com/yegor256/xsline/simple.xsd");
+        Assumptions.assumeTrue(xsd.toFile().exists());
+        MatcherAssert.assertThat(
+            new Xsline(new StSchema()).pass(
+                new XMLDocument(
+                    String.join(
+                        "",
+                        "<foo xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' ",
+                        String.format(
+                            "xsi:noNamespaceSchemaLocation='file://%s'>",
+                            xsd.toFile().getAbsoluteFile()
+                        ),
+                        "42</foo>"
+                    )
+                )
+            ),
+            XhtmlMatchers.hasXPaths("/foo")
         );
     }
 
